826bb8eae384114a9eb2051561836d9aa3634f05
Boris <betttttt@yandex.ru>
2024-02-16 21:27:54 +0300

    get requests without queries

---
+++ app.py
@@ -0,0 +1,43 @@
+import fastapi
+import pydantic_models
+import config
+
+app = fastapi.FastAPI()
+
+response = {"her, pizda, ochko"}
+fake_database = {'users':[
+    {
+        "id":1,             # тут тип данных - число
+        "name":"Anna",      # тут строка
+        "nick":"Anny42",    # и тут
+        "balance": 15300    # а тут float
+     },
+
+    {
+        "id":2,             # у второго пользователя
+        "name":"Dima",      # такие же
+        "nick":"dimon2319", # типы
+        "balance": 160.23     # данных
+     }
+    ,{
+        "id":3,             # у третьего
+        "name":"Vladimir",  # юзера
+        "nick":"Vova777",   # мы специально сделаем
+        "balance": "25000"     # нестандартный тип данных в его балансе
+     }
+],}
+@app.get("/user_balance/{id}")
+def user_balance(id: int):
+    return fake_database["users"][id]["balance"]
+@app.get("/user_info/{id}")
+def user_info(id : int):
+    return fake_database["users"][id]
+@app.get("/total_balance")
+def total_balance():
+    balance_sum: float = 0.0
+    for user in fake_database["users"]:
+        # print(type(user))
+        balance_sum += pydantic_models.Users(**user).balance
+    return balance_sum
+
+
